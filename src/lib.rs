3d6fd3baa9013051c4b864bbb722662fe14216a9
Divy Srivastava <dj.srivastava23@gmail.com>
2024-01-27 10:48:49 +0530

    Use a single include_bytes!

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 #[repr(C, align(16))]
-struct IcuData<const LENGTH: usize>([u8; LENGTH]);
+struct IcuData<T: ?Sized>(T);
 
-const LENGTH: usize = include_bytes!("icudtl.dat").len();
-static ICU_DATA_RAW: IcuData<LENGTH> = IcuData(*include_bytes!("icudtl.dat"));
+static ICU_DATA_RAW: &'static IcuData<[u8]> = &IcuData(*include_bytes!("icudtl.dat"));
 
 /// Raw ICU data.
 pub static ICU_DATA: &[u8] = &ICU_DATA_RAW.0;
