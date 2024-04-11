56f16267cdfbf54efc145ed44a5746fda18496b6
liushuyu <liushuyu011@gmail.com>
2024-04-11 13:32:31 -0600

    icudt*.dat: update binary blobs from ...
    ... https://chromium.googlesource.com/chromium/deps/icu/+/a622de35ac311c5ad390a7af80724634e5dc61ed

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 #[repr(C, align(16))]
 struct IcuData<T: ?Sized>(T);
 
+#[cfg(target_endian="little")]
 static ICU_DATA_RAW: &'static IcuData<[u8]> = &IcuData(*include_bytes!("icudtl.dat"));
+
+#[cfg(target_endian="big")]
+static ICU_DATA_RAW: &'static IcuData<[u8]> = &IcuData(*include_bytes!("icudtb.dat"));
 
 /// Raw ICU data.
 pub static ICU_DATA: &[u8] = &ICU_DATA_RAW.0;
